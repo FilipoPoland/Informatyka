c669dea834fa142305216b6ab772f33466379340
FilipoPoland <40774085+FilipoPoland@users.noreply.github.com>
2023-03-21 17:51:16 +0100

    Commit

--- Jezeli.py
+++ Jezeli.py
@@ -20,15 +20,4 @@
 # y = float(input('Podaj liczbe nr 2:'))
 # print(x+y)
 
-# input polacz w jeden print odzielony spacja dwa pierwsze znaki str zamien na dwa pierwsze drugiego str
 
-i1 = input('Podaj fraze 1')
-i2 = input('Podaj fraze 2')
-print(i1.replace(i1[:2], i2[:2]) + i1, i2.replace(i2[:2], i1[:2]) + i2[2:])
-
-# zapytaj uzytkownika o imie i wiek, powitaj go, wyswietl w ktorym roku bedzie mial sto lat bedzie mial sto lat
-# input('Podaj swoje imie: ')
-# input('Podaj swój wiek: ')
-
-
-# oblicz pole trojkata o wymiarach podanych przez uzytkownika wysokosc i podstawa
