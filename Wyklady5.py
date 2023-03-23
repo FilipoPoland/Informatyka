f75557f3a8fee6eec97c3bd3040dbc658017cb07
FilipoPoland <filipopoland@wp.pl>
2023-03-23 09:59:47 +0100

    Commit

---
+++ Wyklady5.py
@@ -0,0 +1,50 @@
+# x = int(input('Podaj liczbę dla ktorej chcemy policzyć silnię: '))
+#
+# silnia = 1
+# for i in range(1, x+1):
+#     silnia = silnia * i
+#
+# print(silnia)
+
+# x = int(input('Podaj pierwszą wartość, dla której chcsz znależć wspólny dzielnik: '))
+# y = int(input('Podaj drugą wartość, dla której chcsz znależć wspólny dzielnik: '))
+#
+# while x != y:
+#     if x > y:
+#         x -= y
+#     else:
+#         y -= x
+#
+# print(x)
+
+# x = int(input('Podaj pierwszą wartość, dla której chcsz znależć wspólny dzielnik: '))
+# y = int(input('Podaj drugą wartość, dla której chcsz znależć wspólny dzielnik: '))
+#
+# # something is no yes
+# while x != 0:
+#     if x <= y:
+#         x %= y
+#     else:
+#         y %= x
+#
+# if x > y:
+#     print(x)
+# else:
+#     print(y)
+
+# from random import randint
+
+# lista = []
+# for i in range(10):
+#    lista.append((randint(1, 100)))
+
+# print(lista)
+
+# for j in range(9):
+#    for i in range(9):
+#        if lista[i] > lista[i + 1]:
+#            tmp = lista[i]
+#            lista[i] = lista[i + 1]
+#            lista[i] = tmp
+# lista.sort()
+# print(lista)
