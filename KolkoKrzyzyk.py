4d7e4d3bc11f8face30539ab80a48d0cbc3d47cc
FilipoPoland <40774085+FilipoPoland@users.noreply.github.com>
2023-04-03 20:28:28 +0200

    Commit

--- KolkoKrzyzyk.py
+++ KolkoKrzyzyk.py
@@ -16,21 +16,30 @@ def plansza(stan):
     print('+')
 
 
-def sprawdz(getiteam, getiteam2):
+def sprawdz(getiteam, getiteam2, getiteam3):
     # for 0
-    if getiteam[0] == getiteam[1] == getiteam[2] == '0':
+    if getiteam[0] == getiteam[1] == getiteam[2] == 0:
         getiteam2.append('The computer won.')
         return False
-    elif getiteam[3] == getiteam[4] == getiteam[5] == '0':
+    elif getiteam[3] == getiteam[4] == getiteam[5] == 0:
         getiteam2.append('The computer won.')
         return False
-    elif getiteam[6] == getiteam[7] == getiteam[8] == '0':
+    elif getiteam[6] == getiteam[7] == getiteam[8] == 0:
         getiteam2.append('The computer won.')
         return False
-    elif getiteam[0] == getiteam[4] == getiteam[8] == '0':
+    elif getiteam[0] == getiteam[4] == getiteam[8] == 0:
         getiteam2.append('The computer won.')
         return False
-    elif getiteam[2] == getiteam[4] == getiteam[6] == '0':
+    elif getiteam[2] == getiteam[4] == getiteam[6] == 0:
+        getiteam2.append('The computer won.')
+        return False
+    elif getiteam[0] == getiteam[3] == getiteam[6] == 0:
+        getiteam2.append('The computer won.')
+        return False
+    elif getiteam[1] == getiteam[4] == getiteam[7] == 0:
+        getiteam2.append('The computer won.')
+        return False
+    elif getiteam[2] == getiteam[5] == getiteam[8] == 0:
         getiteam2.append('The computer won.')
         return False
     # for X
@@ -49,7 +58,16 @@ def sprawdz(getiteam, getiteam2):
     elif getiteam[2] == getiteam[4] == getiteam[6] == 'X':
         getiteam2.append('You won.')
         return False
-    elif not getiteam:
+    elif getiteam[0] == getiteam[3] == getiteam[6] == 0:
+        getiteam2.append('You won.')
+        return False
+    elif getiteam[1] == getiteam[4] == getiteam[7] == 0:
+        getiteam2.append('You won.')
+        return False
+    elif getiteam[2] == getiteam[5] == getiteam[8] == 0:
+        getiteam2.append('You won.')
+        return False
+    elif not getiteam3:
         getiteam2.append('It is a tie')
         return False
     else:
@@ -61,19 +79,25 @@ list1 = [' '] * 9
 list2 = []
 for i in range(9):
     list2.append(i)
-print(list2)
+
 zwyciestwo = []
 
-while sprawdz(list1, zwyciestwo):
+x = sprawdz(list1, zwyciestwo, list2)
+
+while x:
     shuffle(list2)
     los = list2[0]
     del(list2[0])
     list1[los] = 0
+    y = 1
     plansza(list1)
+    x = sprawdz(list1, zwyciestwo, list2)
+    if x:
+        user = int(input('Podaj pole(zakres 1-9): '))
+        user -= 1
+        list1[user] = 'X'
+        list2.remove(user)
+        x = sprawdz(list1, zwyciestwo, list2)
 
-    user = int(input('Podaj pole(zakres 0 - 8): '))
-    list1[user] = 'X'
-    list2.remove(user)
-    plansza(list1)
 
 print(zwyciestwo[0])
