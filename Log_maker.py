e667cb69169eb44a8359f8bfc942abdedf339b0b
FilipoPoland <filipopoland@wp.pl>
2023-04-12 12:19:35 +0200

    Commit

--- Log_maker.py
+++ Log_maker.py
@@ -1,33 +1,45 @@
+# program tworzenia wpisów do dziennika
 import datetime
 
+print('Dziennik: ')
 condition = True
 list_category = []
 list_date = []
 list_entry = []
 while True:
     while condition:
-        intent = input('Do you wish to make a log entry?(y/n)')
-        if intent == 'y':
-            category = input('What is the category of your log entry?\n'
-                             'Personal / Medical / Engineering / Navigation / Observations / Security')
+        intent = input('Czy chcesz dokonać wpisu do dziennika?(t/n)')
+        if intent == 't':
+            category = input('Jaka jest kategoria Twojego wpisu?')
             # adding the category to the list
             list_category.append(category)
             # assign date
-            list_date.append(datetime.date.today())
+            list_date.append(datetime.datetime.now())
             # asking the user for his/her log entry
-            entry = input('Please provide the log entry: ')
+            entry = input('Podaj swój wpis: ')
             # appending list entry
             list_entry.append(entry)
+            # save all lists
+            with open('Database_logs', 'w') as file:
+                file.write('list_date = ' + str(list_date) + '\n' + 'list_category = ' + str(list_category) + '\n' +
+                           'list_entry = ' + str(list_entry))
         elif intent == 'n':
             condition = False
 
-    display = input('Do you wish to display a log entry?(y/n)')
-    if display == 'y':
-        index = input('Please provide the index of the entry you would like to display')
-        if index == 'all':
-            for i in range(len(list_date)):
-                print('', list_date[0+i], list_category[0+i], list_entry[0+i], sep='\n')
-        else:
-            print('', list_date[int(index)], list_category[int(index)], list_entry[int(index)], sep='\n')
-    elif display == 'n':
-        condition = True
+    display = input('Czy wyświetlić wpisy dziennika?(t/n)')
+    condition2 = True
+    while condition2:
+        if display == 't':
+            kategoria = input('Podaj kategorię wpisu lub wpisz: wszystkie')
+            if kategoria == 'wszystkie':
+                for i in range(len(list_date)):
+                    print('', list_date[0+i], list_category[0+i], list_entry[0+i], sep='\n')
+                    condition2 = False
+            elif kategoria != list_category[kategoria]:
+                print('Kategorii nie ma w bazie danych.')
+                contition2 = True
+            elif kategoria == list_category[kategoria]:
+                print(list_category.index(kategoria))
+                for i in range(list_category.count(kategoria)):
+                    print('', list_date[0+i], list_category[kategoria], list_entry[0+i], sep='\n')
+                    condition2 = False
